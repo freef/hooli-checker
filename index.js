d33931a13f4947797c69e640350a090bb01fbdd0
freef <mtfreeland@gmail.com>
2019-05-15 17:09:41 -0300

    inital commit with data import and cleanup

--- index.js
+++ index.js
@@ -2,7 +2,26 @@ const fs = require('fs'); // useful for navigating the file system
 const parse = require('csv-parse/lib/sync'); // needed for parsing CSV file data
 
 function linkBuyerToFacility() {
-  // your solution goes here
+  let existingHooli = []
+  let samHooli = []
+    // import and parse existing accounts
+    let existingPromise = fs.promises.readFile('./existing-accounts.csv')
+    .then(parse)
+    .then(data => data.forEach(item => existingHooli.push(item[1].slice(0, -3))))
+    .then((data) => existingHooli = existingHooli.sort())
+    .catch(error => reject(error))
+    // import and parse sam accounts
+    const samPromise = fs.promises.readFile('sam-accounts.csv')
+    .then(parse)
+    .then(data => data.forEach(item => samHooli.push(item[0])))
+    .then((data) => samHooli = samHooli.sort())
+    .catch(error => reject(error))
+
+  Promise.all([existingPromise, samPromise])
+  .then(() => console.log(samHooli.length))
 }
 
-linkBuyerToFacility();
+linkBuyerToFacility()
+
+// 0011R00001zEpqn
+// 0011R00001zCcZxQAK
